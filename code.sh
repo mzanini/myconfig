fec65b3491e4a79cd1fcc696000c630a556d42f0
Marco Zanini <chetto87@gmail.com>
2018-02-22 10:23:54 -0800

    Add script to install Visual Studio code on RHEL

---
+++ code.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Imporing VsCode repository"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+echo "Installing VS Code"
+yum check-update
+sudo yum install code
